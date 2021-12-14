ec56f7b4564222e8b1e63a1219607497b62270ea
Emanuel Rodrigues <44001420+Jarbbas@users.noreply.github.com>
2021-12-14 21:03:09 +0000

    primeiro commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# TabelaPeriodica 0.1
+
+Update ao projeto da Tabela Periodica 
+- [@EmanuelRodrigues](https://github.com/Jarbbas/TabelaPeriodica)
+Esta versão "simplificada" usa a framework do jQuery para preencher
+a tabela através dos dados de uma array guardada em javascript
+
+
+## Objectivo 
+
+O objectivo com este projeto é melhorar as skills da linguagem javascript
+e usar a framework jQuery 
+  
+## Autor
+
+- [@EmanuelRodrigues](https://github.com/Jarbbas)
+
+  
+## Licenças
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://choosealicense.com/licenses/mit/)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+
+  
---
+++ js/scripts.js
@@ -0,0 +1,191 @@
+elementos = [
+    { 'numero': '1', 'simbolo': 'H', 'nome': 'Hidrogénio', 'massaatomica': '1,008', 'cor': '#F58634', 'linha': '1', 'coluna': '1' },
+    { 'numero': '2', 'simbolo': 'He', 'nome': 'Hélio', 'massaatomica': '4,0026', 'cor': '#FFCC29', 'linha': '1', 'coluna': '18' },
+    { 'numero': '3', 'simbolo': 'Li', 'nome': 'Lítio', 'massaatomica': '6,94', 'cor': '#ED3237', 'linha': '2', 'coluna': '1' },
+    { 'numero': '4', 'simbolo': 'Be', 'nome': 'Berílio', 'massaatomica': '9,0122', 'cor': '#F7ADAF', 'linha': '2', 'coluna': '2' },
+    { 'numero': '5', 'simbolo': 'B', 'nome': 'Boro', 'massaatomica': '10,81', 'cor': '#A8CF45', 'linha': '2', 'coluna': '13' },
+    { 'numero': '6', 'simbolo': 'C', 'nome': 'Carbono', 'massaatomica': '12,011', 'cor': '#F58634', 'linha': '2', 'coluna': '14' },
+    { 'numero': '7', 'simbolo': 'N', 'nome': 'Nitrogénio', 'massaatomica': '14,007', 'cor': '#F58634', 'linha': '2', 'coluna': '15' },
+    { 'numero': '8', 'simbolo': 'O', 'nome': 'Oxigénio', 'massaatomica': '15,999', 'cor': '#F58634', 'linha': '2', 'coluna': '16' },
+    { 'numero': '9', 'simbolo': 'F', 'nome': 'Flúor', 'massaatomica': '18,998', 'cor': '#57B952', 'linha': '2', 'coluna': '17' },
+    { 'numero': '10', 'simbolo': 'Ne', 'nome': 'Néon', 'massaatomica': '20,18', 'cor': '#FFCC29', 'linha': '2', 'coluna': '18' },
+    { 'numero': '11', 'simbolo': 'Na', 'nome': 'Sódio', 'massaatomica': '22,99', 'cor': '#ED3237', 'linha': '3', 'coluna': '1' },
+    { 'numero': '12', 'simbolo': 'Mg', 'nome': 'Magnésio', 'massaatomica': '24,305', 'cor': '#F7ADAF', 'linha': '3', 'coluna': '2' },
+    { 'numero': '13', 'simbolo': 'Al', 'nome': 'Alumínio', 'massaatomica': '26,982', 'cor': '#D2E28B', 'linha': '3', 'coluna': '13' },
+    { 'numero': '14', 'simbolo': 'Si', 'nome': 'Silício', 'massaatomica': '28,085', 'cor': '#A8CF45', 'linha': '3', 'coluna': '14' },
+    { 'numero': '15', 'simbolo': 'P', 'nome': 'Fósforo', 'massaatomica': '30,974', 'cor': '#F58634', 'linha': '3', 'coluna': '15' },
+    { 'numero': '16', 'simbolo': 'Si', 'nome': 'Enxofre', 'massaatomica': '32,06', 'cor': '#F58634', 'linha': '3', 'coluna': '16' },
+    { 'numero': '17', 'simbolo': 'Cl', 'nome': 'Cloro', 'massaatomica': '35,45', 'cor': '#57B952', 'linha': '3', 'coluna': '17' },
+    { 'numero': '18', 'simbolo': 'Ar', 'nome': 'Árgon', 'massaatomica': '39,95', 'cor': '#FFCC29', 'linha': '3', 'coluna': '18' },
+    { 'numero': '19', 'simbolo': 'K', 'nome': 'Potássio', 'massaatomica': '39,098', 'cor': '#ED3237', 'linha': '4', 'coluna': '1' },
+    { 'numero': '20', 'simbolo': 'Ca', 'nome': 'Cálcio', 'massaatomica': '40,078', 'cor': '#F7ADAF', 'linha': '4', 'coluna': '2' },
+    { 'numero': '21', 'simbolo': 'Sc', 'nome': 'Escândio', 'massaatomica': '44,956', 'cor': '#0098DA', 'linha': '4', 'coluna': '3' },
+    { 'numero': '22', 'simbolo': 'T', 'nome': 'Titânio', 'massaatomica': '47,867', 'cor': '#0098DA', 'linha': '4', 'coluna': '4' },
+    { 'numero': '23', 'simbolo': 'V', 'nome': 'Vanádio', 'massaatomica': '50,942', 'cor': '#0098DA', 'linha': '4', 'coluna': '5' },
+    { 'numero': '24', 'simbolo': 'Cr', 'nome': 'Crómio', 'massaatomica': '51,996', 'cor': '#0098DA', 'linha': '4', 'coluna': '6' },
+    { 'numero': '25', 'simbolo': 'Mn', 'nome': 'Manganésio', 'massaatomica': '54,938', 'cor': '#0098DA', 'linha': '4', 'coluna': '7' },
+    { 'numero': '26', 'simbolo': 'Fe', 'nome': 'Ferro', 'massaatomica': '55,845', 'cor': '#0098DA', 'linha': '4', 'coluna': '8' },
+    { 'numero': '27', 'simbolo': 'Co', 'nome': 'Cobalto', 'massaatomica': '58,933', 'cor': '#0098DA', 'linha': '4', 'coluna': '9' },
+    { 'numero': '28', 'simbolo': 'Ni', 'nome': 'Níquel', 'massaatomica': '58,693', 'cor': '#0098DA', 'linha': '4', 'coluna': '10' },
+    { 'numero': '29', 'simbolo': 'Cu', 'nome': 'Cobre', 'massaatomica': '63,546', 'cor': '#0098DA', 'linha': '4', 'coluna': '11' },
+    { 'numero': '30', 'simbolo': 'Zn', 'nome': 'Zinco', 'massaatomica': '65,38', 'cor': '#0098DA', 'linha': '4', 'coluna': '12' },
+    { 'numero': '31', 'simbolo': 'Ga', 'nome': 'Gálio', 'massaatomica': '69,723', 'cor': '#D2E28B', 'linha': '4', 'coluna': '13' },
+    { 'numero': '32', 'simbolo': 'Ge', 'nome': 'Germânio', 'massaatomica': '72,63', 'cor': '#A8CF45', 'linha': '4', 'coluna': '14' },
+    { 'numero': '33', 'simbolo': 'As', 'nome': 'Arsénio', 'massaatomica': '74,922', 'cor': '#A8CF45', 'linha': '4', 'coluna': '15' },
+    { 'numero': '34', 'simbolo': 'Se', 'nome': 'Selénio', 'massaatomica': '78,971', 'cor': '#F58634', 'linha': '4', 'coluna': '16' },
+    { 'numero': '35', 'simbolo': 'Br', 'nome': 'Bromo', 'massaatomica': '79,904', 'cor': '#57B952', 'linha': '4', 'coluna': '17' },
+    { 'numero': '36', 'simbolo': 'Kr', 'nome': 'Kripton', 'massaatomica': '83,798', 'cor': '#FFCC29', 'linha': '4', 'coluna': '18' },
+    { 'numero': '37', 'simbolo': 'Rb', 'nome': 'Rubídio', 'massaatomica': '85,468', 'cor': '#ED3237', 'linha': '5', 'coluna': '1' },
+    { 'numero': '38', 'simbolo': 'Sr', 'nome': 'Estrôncio', 'massaatomica': '87,62', 'cor': '#F7ADAF', 'linha': '5', 'coluna': '2' },
+    { 'numero': '39', 'simbolo': 'Y', 'nome': 'Ítrio', 'massaatomica': '88,906', 'cor': '#0098DA', 'linha': '5', 'coluna': '3' },
+    { 'numero': '40', 'simbolo': 'Zr', 'nome': 'Zircónio', 'massaatomica': '91,224', 'cor': '#0098DA', 'linha': '5', 'coluna': '4' },
+    { 'numero': '41', 'simbolo': 'Nb', 'nome': 'Nióbio', 'massaatomica': '92,906', 'cor': '#0098DA', 'linha': '5', 'coluna': '5' },
+    { 'numero': '42', 'simbolo': 'Mo', 'nome': 'Molibdénio', 'massaatomica': '95,95', 'cor': '#0098DA', 'linha': '5', 'coluna': '6' },
+    { 'numero': '43', 'simbolo': 'Tc', 'nome': 'Tecnécio', 'massaatomica': '', 'cor': '#0098DA', 'linha': '5', 'coluna': '7' },
+    { 'numero': '44', 'simbolo': 'Ru', 'nome': 'Ruténio', 'massaatomica': '101,07', 'cor': '#0098DA', 'linha': '5', 'coluna': '8' },
+    { 'numero': '45', 'simbolo': 'Rh', 'nome': 'Ródio', 'massaatomica': '102,91', 'cor': '#0098DA', 'linha': '5', 'coluna': '9' },
+    { 'numero': '46', 'simbolo': 'Pd', 'nome': 'Paládio', 'massaatomica': '106,42', 'cor': '#0098DA', 'linha': '5', 'coluna': '10' },
+    { 'numero': '47', 'simbolo': 'Ag', 'nome': 'Prata', 'massaatomica': '107,87', 'cor': '#0098DA', 'linha': '5', 'coluna': '11' },
+    { 'numero': '48', 'simbolo': 'Cd', 'nome': 'Cádmio', 'massaatomica': '112,41', 'cor': '#0098DA', 'linha': '5', 'coluna': '12' },
+    { 'numero': '49', 'simbolo': 'In', 'nome': 'Índio', 'massaatomica': '141,82', 'cor': '#D2E28B', 'linha': '5', 'coluna': '13' },
+    { 'numero': '50', 'simbolo': 'Sn', 'nome': 'Estanho', 'massaatomica': '118,71', 'cor': '#D2E28B', 'linha': '5', 'coluna': '14' },
+    { 'numero': '51', 'simbolo': 'Sb', 'nome': 'Antimónio', 'massaatomica': '121,76', 'cor': '#A8CF45', 'linha': '5', 'coluna': '15' },
+    { 'numero': '52', 'simbolo': 'Te', 'nome': 'Telúrio', 'massaatomica': '127,6', 'cor': '#A8CF45', 'linha': '5', 'coluna': '16' },
+    { 'numero': '53', 'simbolo': 'In', 'nome': 'Iodo', 'massaatomica': '126,9', 'cor': '#57B952', 'linha': '5', 'coluna': '17' },
+    { 'numero': '54', 'simbolo': 'Xe', 'nome': 'Xenon', 'massaatomica': '131,29', 'cor': '#FFCC29', 'linha': '5', 'coluna': '18' },
+    { 'numero': '55', 'simbolo': 'Cs', 'nome': 'Césio', 'massaatomica': '132,91', 'cor': '#ED3237', 'linha': '6', 'coluna': '1' },
+    { 'numero': '56', 'simbolo': 'Ba', 'nome': 'Bário', 'massaatomica': '137,33', 'cor': '#F7ADAF', 'linha': '6', 'coluna': '2' },
+    { 'numero': '57', 'simbolo': 'La', 'nome': 'Lantânio', 'massaatomica': '138,91', 'cor': '#40B7EE', 'linha': '9', 'coluna': '4' },
+    { 'numero': '58', 'simbolo': 'Ce', 'nome': 'Cério', 'massaatomica': '140,12', 'cor': '#40B7EE', 'linha': '9', 'coluna': '5' },
+    { 'numero': '59', 'simbolo': 'Pr', 'nome': 'Praseodímio', 'massaatomica': '140,91', 'cor': '#40B7EE', 'linha': '9', 'coluna': '6' },
+    { 'numero': '60', 'simbolo': 'Nd', 'nome': 'Neodímio', 'massaatomica': '144,24', 'cor': '#40B7EE', 'linha': '9', 'coluna': '7' },
+    { 'numero': '61', 'simbolo': 'Pm', 'nome': 'Promécio', 'massaatomica': '', 'cor': '#40B7EE', 'linha': '9', 'coluna': '8' },
+    { 'numero': '62', 'simbolo': 'Sm', 'nome': 'Samário', 'massaatomica': '150,36', 'cor': '#40B7EE', 'linha': '9', 'coluna': '9' },
+    { 'numero': '63', 'simbolo': 'Eu', 'nome': 'Európio', 'massaatomica': '151,96', 'cor': '#40B7EE', 'linha': '9', 'coluna': '10' },
+    { 'numero': '64', 'simbolo': 'Gd', 'nome': 'Gadolínio', 'massaatomica': '157,25', 'cor': '#40B7EE', 'linha': '9', 'coluna': '11' },
+    { 'numero': '65', 'simbolo': 'Tb', 'nome': 'Térbio', 'massaatomica': '158,93', 'cor': '#40B7EE', 'linha': '9', 'coluna': '12' },
+    { 'numero': '66', 'simbolo': 'Dy', 'nome': 'Disprósio', 'massaatomica': '162,5', 'cor': '#40B7EE', 'linha': '9', 'coluna': '13' },
+    { 'numero': '67', 'simbolo': 'Ho', 'nome': 'Hólmio', 'massaatomica': '164,93', 'cor': '#40B7EE', 'linha': '9', 'coluna': '14' },
+    { 'numero': '68', 'simbolo': 'Er', 'nome': 'Érbio', 'massaatomica': '167,26', 'cor': '#40B7EE', 'linha': '9', 'coluna': '15' },
+    { 'numero': '69', 'simbolo': 'Tm', 'nome': 'Túlio', 'massaatomica': '168,93', 'cor': '#40B7EE', 'linha': '9', 'coluna': '16' },
+    { 'numero': '70', 'simbolo': 'Yb', 'nome': 'Itérbio', 'massaatomica': '173,05', 'cor': '#40B7EE', 'linha': '9', 'coluna': '17' },
+    { 'numero': '71', 'simbolo': 'Lu', 'nome': 'Lutécio', 'massaatomica': '174,97', 'cor': '#40B7EE', 'linha': '9', 'coluna': '18' },
+    { 'numero': '72', 'simbolo': 'Hf', 'nome': 'Háfnio', 'massaatomica': '178,49', 'cor': '#0098DA', 'linha': '6', 'coluna': '4' },
+    { 'numero': '73', 'simbolo': 'Ta', 'nome': 'Tantalo', 'massaatomica': '180,95', 'cor': '#0098DA', 'linha': '6', 'coluna': '5' },
+    { 'numero': '74', 'simbolo': 'W', 'nome': 'Tungsténio', 'massaatomica': '183,84', 'cor': '#0098DA', 'linha': '6', 'coluna': '6' },
+    { 'numero': '75', 'simbolo': 'Re', 'nome': 'Rénio', 'massaatomica': '186,23', 'cor': '#0098DA', 'linha': '6', 'coluna': '7' },
+    { 'numero': '76', 'simbolo': 'Os', 'nome': 'Ósmio', 'massaatomica': '190,23', 'cor': '#0098DA', 'linha': '6', 'coluna': '8' },
+    { 'numero': '77', 'simbolo': 'Ir', 'nome': 'Íridio', 'massaatomica': '192,22', 'cor': '#0098DA', 'linha': '6', 'coluna': '9' },
+    { 'numero': '78', 'simbolo': 'Pt', 'nome': 'Platina', 'massaatomica': '195,08', 'cor': '#0098DA', 'linha': '6', 'coluna': '10' },
+    { 'numero': '79', 'simbolo': 'Au', 'nome': 'Ouro', 'massaatomica': '196,97', 'cor': '#0098DA', 'linha': '6', 'coluna': '11' },
+    { 'numero': '80', 'simbolo': 'Hg', 'nome': 'Mercúrio', 'massaatomica': '200,59', 'cor': '#0098DA', 'linha': '6', 'coluna': '12' },
+    { 'numero': '81', 'simbolo': 'TI', 'nome': 'Tálio', 'massaatomica': '204,38', 'cor': '#D2E28B', 'linha': '6', 'coluna': '13' },
+    { 'numero': '82', 'simbolo': 'Pb', 'nome': 'Chumbo', 'massaatomica': '207,2', 'cor': '#D2E28B', 'linha': '6', 'coluna': '14' },
+    { 'numero': '83', 'simbolo': 'Bi', 'nome': 'Bismuto', 'massaatomica': '208,98', 'cor': '#D2E28B', 'linha': '6', 'coluna': '15' },
+    { 'numero': '84', 'simbolo': 'Po', 'nome': 'Polónio', 'massaatomica': '', 'cor': '#A8CF45', 'linha': '6', 'coluna': '16' },
+    { 'numero': '85', 'simbolo': 'At', 'nome': 'Astato', 'massaatomica': '', 'cor': '#57B952', 'linha': '6', 'coluna': '17' },
+    { 'numero': '86', 'simbolo': 'Rn', 'nome': 'Rádon', 'massaatomica': '', 'cor': '#FFCC29', 'linha': '6', 'coluna': '18' },
+    { 'numero': '87', 'simbolo': 'Fr', 'nome': 'Frâncio', 'massaatomica': '', 'cor': '#ED3237', 'linha': '7', 'coluna': '1' },
+    { 'numero': '88', 'simbolo': 'Ra', 'nome': 'Rádio', 'massaatomica': '', 'cor': '#F7ADAF', 'linha': '7', 'coluna': '2' },
+    { 'numero': '89', 'simbolo': 'Ac', 'nome': 'Actínio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '4' },
+    { 'numero': '90', 'simbolo': 'Th', 'nome': 'Tório', 'massaatomica': '232,04', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '5' },
+    { 'numero': '91', 'simbolo': 'Pa', 'nome': 'Protactínio', 'massaatomica': '231,04', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '6' },
+    { 'numero': '92', 'simbolo': 'U', 'nome': 'Urânio', 'massaatomica': '238,03', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '7' },
+    { 'numero': '93', 'simbolo': 'Np', 'nome': 'Neptunio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '8' },
+    { 'numero': '94', 'simbolo': 'Pu', 'nome': 'Plutónio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '9' },
+    { 'numero': '95', 'simbolo': 'Am', 'nome': 'Amerício', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '10' },
+    { 'numero': '96', 'simbolo': 'Cm', 'nome': 'Cúrio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '11' },
+    { 'numero': '97', 'simbolo': 'Bk', 'nome': 'Berquélio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '12' },
+    { 'numero': '98', 'simbolo': 'Cf', 'nome': 'Califórnio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '13' },
+    { 'numero': '99', 'simbolo': 'Es', 'nome': 'Einsteinio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '14' },
+    { 'numero': '100', 'simbolo': 'Fm', 'nome': 'Férmio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '15' },
+    { 'numero': '101', 'simbolo': 'Md', 'nome': 'Medelévio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '16' },
+    { 'numero': '102', 'simbolo': 'No', 'nome': 'Nobélio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '17' },
+    { 'numero': '103', 'simbolo': 'Lr', 'nome': 'Laurêncio', 'massaatomica': '', 'cor': '#9AD8F9', 'linha': '10', 'coluna': '18' },
+    { 'numero': '104', 'simbolo': 'Rf', 'nome': 'Rutherfórdio', 'massaatomica': '', 'cor': '#0098DA', 'linha': '7', 'coluna': '4' },
+    { 'numero': '105', 'simbolo': 'Db', 'nome': 'Dúbnio', 'massaatomica': '', 'cor': '#0098DA', 'linha': '7', 'coluna': '5' },
+    { 'numero': '106', 'simbolo': 'Sg', 'nome': 'Seabórgio', 'massaatomica': '', 'cor': '#0098DA', 'linha': '7', 'coluna': '6' },
+    { 'numero': '107', 'simbolo': 'Bh', 'nome': 'Bório', 'massaatomica': '', 'cor': '#0098DA', 'linha': '7', 'coluna': '7' },
+    { 'numero': '108', 'simbolo': 'Hs', 'nome': 'Hássio', 'massaatomica': '', 'cor': '#0098DA', 'linha': '7', 'coluna': '8' },
+    { 'numero': '109', 'simbolo': 'Mt', 'nome': 'Meitnério', 'massaatomica': '', 'cor': '#0098DA', 'linha': '7', 'coluna': '9' },
+    { 'numero': '110', 'simbolo': 'Ds', 'nome': 'Darmstádio', 'massaatomica': '', 'cor': '#0098DA', 'linha': '7', 'coluna': '10' },
+    { 'numero': '111', 'simbolo': 'Rg', 'nome': 'Roentgénio', 'massaatomica': '', 'cor': '#0098DA', 'linha': '7', 'coluna': '11' },
+    { 'numero': '112', 'simbolo': 'Cn', 'nome': 'Copernício', 'massaatomica': '', 'cor': '#0098DA', 'linha': '7', 'coluna': '12' },
+    { 'numero': '113', 'simbolo': 'Nh', 'nome': 'Niónio', 'massaatomica': '', 'cor': '#D2E28B', 'linha': '7', 'coluna': '13' },
+    { 'numero': '114', 'simbolo': 'FL', 'nome': 'Leróvio', 'massaatomica': '', 'cor': '#D2E28B', 'linha': '7', 'coluna': '14' },
+    { 'numero': '115', 'simbolo': 'Mc', 'nome': 'Moscóvio', 'massaatomica': '', 'cor': '#D2E28B', 'linha': '7', 'coluna': '15' },
+    { 'numero': '116', 'simbolo': 'Lv', 'nome': 'Livermório', 'massaatomica': '', 'cor': '#D2E28B', 'linha': '7', 'coluna': '16' },
+    { 'numero': '117', 'simbolo': 'Ts', 'nome': 'Tennesso', 'massaatomica': '', 'cor': '#57B952', 'linha': '7', 'coluna': '17' },
+    { 'numero': '118', 'simbolo': 'Og', 'nome': 'Oganessónio', 'massaatomica': '', 'cor': '#FFCC29', 'linha': '7', 'coluna': '18' }
+];
+
+$(document).ready(function () {
+
+    /** Função para reduzir o Zoom do ecrã, só para a tabela ficar centrada no ecrã */
+    function toggleZoomScreen() {
+        document.body.style.zoom = "67%";
+    } 
+
+    /** Através do Jquery vamos primeiro fazer um append de um elemento <table>
+     *  dentro da <div> com o ID "tabela"
+     */
+    $("#tabela").append("<tabela class='tabelaPeriodica'></tabela>"); 
+
+    /** ciclo para criar 9 linhas e cada ciclo fazer um append de um elemento <tr> com o jQuery */
+    for (var i = 1; i <= 9; i++) {
+        $(".tabelaPeriodica").append("<tr class='linhaElementos'></tr>");
+
+        /** quando o contador i chegar à linha 7 acrescenta uma linha em branco */
+        if(i == 7){ $(".tabelaPeriodica").append("<tr class='linhaBranca'><td><br></td></tr>"); }
+    }
+
+    /** ciclo para criar 18 colunas e fazer um append a cada elemento <tr> com o jQuery */
+    for (var j = 1; j <= 18; j++) {
+        $(".linhaElementos").append("<td class='elemento'></td>");
+    }
+
+    /**
+     * Todos os elementos têm uma class para ser mais fácil diferenciar, 
+     * ou mais tarde caso seja preciso anexar algum CSS extra
+     * <<Exemplo da class : elemento>>
+     */
+    $(".elemento").append("<h1 class='elementoSimbolo'></h1>");
+    $(".elementoSimbolo").after("<h4 class='elementoNome'></h4>");
+    $(".elementoNome").after("<span class='elementoNumero'></span>");
+    $(".elementoNumero").after("<span class='elementoMassaAtomica'></span>");
+
+    /**
+     * Ciclo para percorrer todo o array "elementos" 
+     * Notas:
+     * O tamanho dos tem que ser (elementos.lengh -1) já que começar no registo 0
+     * Tal como as variveis "linha" e "coluna"
+     * De forma a saber a sua posição exata
+     */
+    for (elemento = 0; elemento <= elementos.length - 1; elemento++) {
+        linha = elementos[elemento].linha - 1;
+        coluna = elementos[elemento].coluna - 1;
+        /**
+         * A variavel "celula" vai procurar o "primeiro", "filho" do elemento da class "tabelaPeriodca"
+         * e através da função eq() do jQuery podemos recolher os dados da array para preencher
+         */
+        celula = $(".tabelaPeriodica").first().children().eq(linha).children().eq(coluna);
+
+        /** Adiciona o conteudo Css a cada <td> e seus filhos  */
+        $(celula).css({ "border": "solid 1px black", "background-color": elementos[elemento].cor });
+        /** Procura o elemento <h1> e adiciona o texto do array  */
+        $(celula).find("h1").text(elementos[elemento].simbolo);
+        /** Procura o elemento <h4> e adiciona o texto do array  */
+        $(celula).find("h4").text(elementos[elemento].nome);
+        /** Procura o primeiro elemento <span> e adiciona o texto do array  */
+        $(celula).find("span").first().text(elementos[elemento].numero);
+        /** Procura o primeiro elemento <span> e adiciona um elemento <br>  */
+        $(celula).find("span").first().append("<br>");
+        /** Procura o ultimo elemento <span> e adiciona o texto do array  */
+        $(celula).find("span").last().text(elementos[elemento].massaatomica);
+
+        /**Para efeitos de validação na consola */
+        console.log("Linha: " + linha +"\n");
+        console.log("Coluna: " + coluna +"\n");
+    }
+
+});
